24d9020f8384dc51398defc484fbf1e33825d926
Ferran Carrascosa <ferran.carrascosa@gmail.com>
2020-09-12 13:27:28 +0200

    Formatos

---
+++ www/ConvertirNbaRmd.R
@@ -0,0 +1,14 @@
+
+# https://github.com/rstudio/rmarkdown/blob/master/R/jupyter.R
+source("www/jupyter.R")
+
+convert_ipynb("Python/modulo1_tema4_Py_00_indice.ipynb")
+convert_ipynb("Python/modulo1_tema4_Py.ipynb")
+convert_ipynb("Python/modulo1_tema4_Py_10_elem_bas.ipynb")
+convert_ipynb("Python/modulo1_tema4_Py_20_matplotlib.ipynb")
+convert_ipynb("Python/modulo1_tema4_Py_30_colec_obj.ipynb")
+convert_ipynb("Python/modulo1_tema4_Py_31_numpy.ipynb")
+convert_ipynb("Python/modulo1_tema4_Py_32_pandas.ipynb")
+convert_ipynb("Python/modulo1_tema4_Py_40_contr_flujo.ipynb")
+convert_ipynb("Python/modulo1_tema4_Py_50_gest_dat.ipynb")
+convert_ipynb("Python/modulo1_tema4_Py_60_anexo.ipynb")
